56cf0b7b8080912e8bc2f3d2beb8ebc178e173c4
Matilde Carvalho <uc2019233490@users.noreply.github.com>
2022-05-08 23:21:24 +0100

    Login user (ainda incompleto) - Com mensagens de erro username e Password - Mensagens para menus admin e cliente -Ainda por testar

--- Client_Folder/client.c
+++ Client_Folder/client.c
@@ -43,35 +43,100 @@ int main(int argc, char *argv[])
     if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
         erro("Connect");
 
-    //Mensagem Inicial
+    while(1) {
+        //Mensagem Inicial
 
-    char msg_inicial[BUF_SIZE];
-    recv(fd, msg_inicial, BUF_SIZE, 0);
-    printf("%s\n", msg_inicial);
+        char msg_inicial[BUF_SIZE];
+        recv(fd, msg_inicial, BUF_SIZE, 0);
+        printf("%s\n", msg_inicial);
 
-    //Mensagem a pedir username
-    char msg_get_user[BUF_SIZE];
-    recv(fd, msg_get_user, BUF_SIZE, 0);
-    printf("%s\n", msg_get_user);
+        //Mensagem a pedir username
+        char msg_get_user[BUF_SIZE];
+        recv(fd, msg_get_user, BUF_SIZE, 0);
+        printf("%s\n", msg_get_user);
 
-    //Enviar username
-    char username[BUF_SIZE];
-    scanf("%s", &username);
-    send(fd, username, strlen(username), 0);
-    fflush(stdout);
+        //Enviar username
+        char username[BUF_SIZE];
+        scanf("%s", &username);
+        send(fd, username, strlen(username), 0);
+        fflush(stdout);
 
-    //Mensagem a pedir Password
-    char msg_get_password[BUF_SIZE];
-    recv(fd, msg_get_user, BUF_SIZE, 0);
-    printf("%s\n", msg_get_user);
+        //Aqui qualquer coisa se username não existe e o programa acaba
+        char msg_erro[BUF_SIZE];
+        recv(fd, msg_erro, strlen(msg_erro), 0);
 
-    //Enviar Passowrd
-    char password[BUF_SIZE];
-    scanf("%s", &password);
-    send(fd, username, strlen(password), 0);
-    fflush(stdout);
+        if (strcmp(msg_erro, "  Username não existe. Insira um username válido\n") == 0) {
+            printf("%s", msg_erro);
+            break;
+
+        } else {
+            //Mensagem a pedir Password
+            /*char msg_get_password[BUF_SIZE];
+            recv(fd, msg_get_password, BUF_SIZE, 0);
+            printf("%s\n", msg_get_password);*/
+            printf("%s\n", msg_erro);
+
+            //Enviar Password
+            char password[BUF_SIZE];
+            scanf("%s", &password);
+            send(fd, username, strlen(password), 0);
+            fflush(stdout);
+
+            //ERRO NA PASSWORD
+            char msg_erro[BUF_SIZE];
+            recv(fd, msg_erro, strlen(msg_erro), 0);
+
+            //3 tentativas restantes
+            if (strcmp(msg_erro, "  Password Incorrreta. 3 tentativas restantes!\n") == 0) {
+                printf("%s", msg_erro);
+
+                char msg_get_password[BUF_SIZE];
+                recv(fd, msg_get_password, BUF_SIZE, 0);
+                printf("%s\n", msg_get_password);
+
+                char password[BUF_SIZE];
+                scanf("%s", &password);
+                send(fd, username, strlen(password), 0);
+                fflush(stdout);
+
+                if (strcmp(msg_erro, "  Password Incorrreta. 2 tentativas restantes!\n") == 0) {
+
+                    printf(msg_get_password);
+
+                    char password[BUF_SIZE];
+                    scanf("%s", &password);
+                    send(fd, username, strlen(password), 0);
+                    fflush(stdout);
+
+                    if (strcmp(msg_erro, "  Password Incorrreta. 1 tentativas restantes!\n") == 0) {
+
+                        printf(msg_get_password);
+
+                        char password[BUF_SIZE];
+                        scanf("%s", &password);
+                        send(fd, username, strlen(password), 0);
+                        fflush(stdout);
+
+                    }
+
+                }
 
 
+            }
+
+            if (strcmp(username, "admin") == 0) {
+                char menu_admin[BUF_SIZE];
+                recv(fd, menu_admin, strlen(menu_admin), 0);
+                printf("%s", menu_admin);
+
+            } else {
+                char menu_cliente[BUF_SIZE];
+                recv(fd, menu_cliente, strlen(menu_cliente), 0);
+                printf("%s", menu_cliente);
+            }
+
+        }
+    }
 
 
     close(fd);
